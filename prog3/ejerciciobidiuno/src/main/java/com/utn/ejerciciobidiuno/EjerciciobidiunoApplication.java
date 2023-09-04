eaa86446d8262890ee04909da98bca0564ba213a
JoaquinNavio <joaconavio10@gmail.com>
2023-09-04 09:02:43 -0300

    a

---
+++ prog3/ejerciciobidiuno/src/main/java/com/utn/ejerciciobidiuno/EjerciciobidiunoApplication.java
@@ -0,0 +1,13 @@
+package com.utn.ejerciciobidiuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciobidiunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciobidiunoApplication.class, args);
+	}
+
+}
---
+++ prog3/ejerciciobidiuno/src/test/java/com/utn/ejerciciobidiuno/EjerciciobidiunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.ejerciciobidiuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciobidiunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
