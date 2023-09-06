2aa26b90f353ef243ed1ff255e75920f40b50d05
JoaquinNavio <joaconavio10@gmail.com>
2023-09-06 09:58:28 -0300

    bidi

--- prog3/ejerciciobidiuno/src/main/java/com/utn/ejerciciobidiuno/EjerciciobidiunoApplication.java
+++ prog3/ejerciciobidiuno/src/main/java/com/utn/ejerciciobidiuno/EjerciciobidiunoApplication.java
@@ -1,13 +1,77 @@
 package com.utn.ejerciciobidiuno;
 
+import com.utn.ejerciciobidiuno.entidades.Domicilio;
+import com.utn.ejerciciobidiuno.entidades.Persona;
+import com.utn.ejerciciobidiuno.repositorios.DomicilioRepository;
+import com.utn.ejerciciobidiuno.repositorios.PersonaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EjerciciobidiunoApplication {
+
+	@Autowired
+	PersonaRepository personaRepository;
+	@Autowired
+	DomicilioRepository domicilioRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EjerciciobidiunoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(PersonaRepository personaRepo, DomicilioRepository domicilioRepo) {
+		return args -> {
+			System.out.println("-----------------ESTOY FUNCIONANDO---------");
+
+
+			Domicilio domicilio = Domicilio.builder()
+					.calle("Suipacha")
+					.numero(30)
+					.build();
+			Persona persona = Persona.builder()
+					.nombre("Juan")
+					.apellido("Pérez")
+					.edad(30)
+					.build();
+			// Asocio el domicilio
+			persona.setDomicilio(domicilio);
+			// Asocio la persona
+			domicilio.setPersona(persona);
+			// Guardar el objeto Persona en la base de datos
+			personaRepository.save(persona);
+
+
+			// Recuperar el objeto Persona desde la base de datos
+
+			Persona personaRecuperada = personaRepository.findById(persona.getId()).orElse(null);
+			if (personaRecuperada != null) {
+				System.out.println("Nombre: " + personaRecuperada.getNombre());
+				System.out.println("Apellido: " + personaRecuperada.getApellido());
+				System.out.println("Edad: " + personaRecuperada.getEdad());
+				System.out.println("Calle : " + personaRecuperada.getDomicilio().getCalle());
+				System.out.println("Número :" + personaRecuperada.getDomicilio().getNumero());
+			}
+
+			System.out.println("......  Muestro la bidireccionalidad.......");
+			// Recuperar el objeto Persona desde la base de datos
+			Domicilio domicilioRecuperado = domicilioRepository.findById(persona.getId()).orElse(null);
+			if (domicilioRecuperado!= null) {
+				System.out.println("Nombre: " + domicilioRecuperado.getPersona().getNombre());
+				System.out.println("Apellido: " + domicilioRecuperado.getPersona().getApellido());
+				System.out.println("Edad: " + domicilioRecuperado.getPersona().getEdad());
+				System.out.println("Calle : " + domicilioRecuperado.getCalle());
+				System.out.println("Número :" + domicilioRecuperado.getNumero());
+			}
+
+
+
+		};
+
+	}
+
 }
+
--- prog3/ejerciciobidiuno/src/test/java/com/utn/ejerciciobidiuno/EjerciciobidiunoApplicationTests.java
+++ prog3/ejerciciobidiuno/src/test/java/com/utn/ejerciciobidiuno/EjerciciobidiunoApplicationTests.java
@@ -8,6 +8,7 @@ class EjerciciobidiunoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("ANDO ANDO ANDO DOMINANDO");
 	}
 
 }
