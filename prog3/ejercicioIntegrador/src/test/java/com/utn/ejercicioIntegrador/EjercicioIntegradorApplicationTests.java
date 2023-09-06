8d0beb872c47821242009d52a4638f8226e6249b
JoaquinNavio <joaconavio10@gmail.com>
2023-09-06 12:36:16 -0300

    creacion

---
+++ prog3/ejercicioIntegrador/src/test/java/com/utn/ejercicioIntegrador/EjercicioIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.ejercicioIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
