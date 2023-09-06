6966b5b615709b6cc215eeae4c948210fb36896c
JoaquinNavio <joaconavio10@gmail.com>
2023-09-06 10:37:27 -0300

    create

---
+++ prog3/ejerciciounoaMuchouni/src/main/java/com/utn/ejerciciounoaMuchouni/EjerciciounoaMuchouniApplication.java
@@ -0,0 +1,13 @@
+package com.utn.ejerciciounoaMuchouni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciounoaMuchouniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciounoaMuchouniApplication.class, args);
+	}
+
+}
